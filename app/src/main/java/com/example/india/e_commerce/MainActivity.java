a4980123f67549e0e705df303ce77fdc9c03cffc
realmohdali <realmohdali@gmail.com>
2018-09-01 12:46:01 +0530

    open nav drawer from menu icon added cart icon in toolbar

--- app/src/main/java/com/example/india/e_commerce/MainActivity.java
+++ app/src/main/java/com/example/india/e_commerce/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.india.e_commerce;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
         String name[] = {"Samsung Desktop", "Google Pixel", "LG OLED TV"};
         int img[] = {R.drawable.desktop, R.drawable.pixel, R.drawable.tv};
@@ -29,4 +41,20 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.Adapter adapter = new myAdapter(name, img, amt);
         recyclerView.setAdapter(adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
